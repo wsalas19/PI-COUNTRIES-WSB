f55c529ce17a58ddb6d5bc38dc8bc5bc0b338f56
William Salas <willbullet52@gmail.com>
2022-11-21 10:36:36 -0500

    redux actions done

---
+++ PI-Countries-main/client/src/redux/actions.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import {
+	FILTER_COUNTRIES,
+	GET_COUNTRIES,
+	GET_COUNTRIES_BY_NAME,
+	ORDER_COUNTRIES_ALF,
+	ORDER_COUNTRIES_POP,
+	GET_TOURIST_ACTIVITIES,
+	GET_COUNTRY_DETAIL,
+	GET_COUNTRIES_QUERY,
+	FILTER_BY_ACTIVITIES,
+} from "./types";
+//Getters for country
+export const getCountries = () => async (dispatch) => {
+	try {
+		let info = await axios.get("https://localhost:3001/countries");
+		return dispatch({
+			type: GET_COUNTRIES,
+			payload: info.data,
+		});
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+export const getCountryDetail = (id) => async (dispatch) => {
+	try {
+		let detail = await axios.get(`https://localhost:3001/countries/${id}`);
+		return dispatch({
+			type: GET_COUNTRY_DETAIL,
+			payload: detail.data,
+		});
+	} catch (error) {
+		console.log(`${error.name}:${error.message}`);
+	}
+};
+export const getCountrySearch = (name) => async (dispatch) => {
+	try {
+		let search = await axios.get(
+			`https://localhost:3001/countries/?name=${
+				name.charAt(0).toUpperCase() + name.slice(1)
+			}`
+		);
+		return dispatch({
+			type: GET_COUNTRIES_QUERY,
+			payload: search.data,
+		});
+	} catch (error) {
+		console.log(`${error.name}:${error.message}`);
+	}
+};
+export const getCountriesByName = (name) => {
+	return {
+		type: GET_COUNTRIES_BY_NAME,
+		payload: name,
+	};
+};
+//Get and post for activity db.
+export const getActivities = () => async (dispatch) => {
+	try {
+		let activities = await axios.get("https://localhost:3001/activities");
+		return dispatch({
+			type: GET_TOURIST_ACTIVITIES,
+			payload: activities.data,
+		});
+	} catch (error) {
+		console.log(`${error.name}:${error.message}`);
+	}
+};
+
+export const postActivity = (activityInfo) => async (dispatch) => {
+	const response = await axios.post(
+		"https://localhost:3001/activities",
+		activityInfo
+	);
+	return response;
+};
+//filters and order
+export const filterByActivity = (activity) => {
+	return {
+		type: FILTER_BY_ACTIVITIES,
+		payload: activity,
+	};
+};
+
+export const filterByContinent = (continent) => {
+	return {
+		type: FILTER_COUNTRIES,
+		payload: continent,
+	};
+};
+
+export const orderByName = (name) => {
+	return {
+		type: ORDER_COUNTRIES_ALF,
+		payload: name,
+	};
+};
+export const orderByPop = (payload) => {
+	return {
+		type: ORDER_COUNTRIES_POP,
+		payload,
+	};
+};
--- PI-Countries-main/client/src/redux/reducer.js
+++ PI-Countries-main/client/src/redux/reducer.js
@@ -19,6 +19,126 @@ const initialState = {
 	detail: {},
 };
 
-function rootReducer(state = initialState, action) {}
+function rootReducer(state = initialState, action) {
+	switch (action.type) {
+		// country getters
+		case GET_COUNTRIES:
+			return {
+				...state,
+				countries: action.payload,
+				allCountries: action.payload,
+			};
+		case GET_COUNTRIES_BY_NAME: {
+			let nombre =
+				action.payload === ""
+					? state.allCountries
+					: state.countries.filter((e) =>
+							e.name.toLowerCase().includes(action.payload.toLowerCase())
+					  );
+			console.log(action.payload);
+			return {
+				...state,
+				countries: nombre,
+			};
+		}
+		case GET_COUNTRIES_QUERY:
+			return {
+				...state,
+				countries: action.payload,
+			};
+		case GET_COUNTRY_DETAIL:
+			return {
+				...state,
+				detail: action.payload,
+			};
+		//activities actions
+		case ADD_TOURIST_ACTIVITIES:
+			return {
+				...state,
+			};
+		case GET_TOURIST_ACTIVITIES:
+			return {
+				...state,
+				allActivities: action.payload,
+			};
+		//order and filters
+		case FILTER_COUNTRIES:
+			const totalCountries = state.allCountries;
+			const countriesFiltered =
+				action.payload === "All"
+					? totalCountries
+					: totalCountries.filter((c) => c.continent === action.payload);
+			return {
+				...state,
+				countries: countriesFiltered,
+			};
+		case FILTER_BY_ACTIVITIES:
+			const allCountries2 = state.allCountries;
+
+			const solo = allCountries2.filter((pais) => {
+				return pais.Activities.length > 0;
+			});
+
+			let array = [];
+
+			for (let i = 0; i < solo.length; i++) {
+				for (let j = 0; j < solo[i].Activities.length; j++) {
+					if (solo[i].Activities[j].name === action.payload) {
+						array.push(solo[i]);
+					}
+				}
+			}
+
+			const filtro = action.payload === "Todos" ? allCountries2 : array;
+
+			return {
+				...state,
+				countries: filtro,
+			};
+
+		case ORDER_COUNTRIES_ALF:
+			const orderedAlf =
+				action.payload === "A-Z"
+					? state.countries.sort((a, b) => {
+							if (a.name > b.name) {
+								return 1;
+							}
+							if (a.name < b.name) {
+								return -1;
+							}
+							return 0;
+					  })
+					: state.countries.sort((a, b) => {
+							if (a.name > b.name) {
+								return -1;
+							}
+							if (a.name < b.name) {
+								return 1;
+							}
+							return 0;
+					  });
+			return {
+				...state,
+				countries: orderedAlf,
+			};
+		case ORDER_COUNTRIES_POP:
+			const orderPop =
+				action.payload === "+Population"
+					? state.countries.sort((a, b) => {
+							return a.population - b.population;
+					  })
+					: state.countries.sort((a, b) => {
+							return a.population - b.population;
+					  });
+
+			return {
+				state,
+				countries: orderPop,
+			};
+
+		default:
+			return state;
+	}
+}
 
 export default rootReducer;
